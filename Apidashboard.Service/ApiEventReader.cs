ae161c8220e837f21947f97c1318c2fc33614ae1
Mike Reust <reustmd@gmail.com>
2013-05-05 01:46:43 -0400

    -Cleanup rabbitmq error handling (it ain't sexy, but it'll get the job done for a demo) -Remove missing img from css -Using older version of linq.js that uses caps on methods

--- Apidashboard.Service/ApiEventReader.cs
+++ Apidashboard.Service/ApiEventReader.cs
@@ -42,60 +42,70 @@ namespace ApiDashboard.Service
 
 		private static void InitReader(object state)
 		{
-			var factory = new ConnectionFactory
+			while (true)
 			{
-				//production information left out of repo
-				HostName = "",
-				UserName = "",
-				Password = "",
-				RequestedHeartbeat = 10,
-				VirtualHost = "/",
-				Protocol = Protocols.FromEnvironment(),
-				Port = AmqpTcpEndpoint.UseDefaultPort
-			};
-
-
-			using (var conn = factory.CreateConnection())
-			using (var channel = conn.CreateModel())
-			{
-
-				channel.ExchangeDeclare(ExchangeName, ExchangeType.Topic, true, false, null);
-
-				var queueName = String.Empty;	//queue will be given random name, since it's non-durable we don't need to have a known queue name
-				var durable = false;
-				var exclusive = true;
-				var autoDelete = true;
-				var noAck = true;	//these are non-critical and don't need ack.  Avoiding ack saves server and client some work
-
-
-				//durable (true) -> the queue will be persisted across a rabbitMQ node reboot
-				//exclusive (true) -> the queue can only be accessed via the current connection
-				//auto-delete (true) -> the queue will be deleted once the client disconnects
-				var queue = channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
-				var routingKey = "ApiWebRequestCompletedEvent";
-
-				channel.QueueBind(queueName, ExchangeName, routingKey);
-
-				var consumer = new QueueingBasicConsumer(channel);
-				channel.BasicConsume(queueName, noAck, consumer);
-
-				BasicDeliverEventArgs envelope;
-
-				while (true)
+				try
 				{
-					object _ = null;
-					if (!consumer.Queue.Dequeue(500, out _)) //release poll at 500 ms
-						continue;
+					var factory = new ConnectionFactory
+						{
+							//production information left out of repo
+							HostName = "rabbitmqprod.groupcommerce.com",
+							UserName = "api",
+							Password = "",
+							RequestedHeartbeat = 10,
+							VirtualHost = "/",
+							Protocol = Protocols.FromEnvironment(),
+							Port = AmqpTcpEndpoint.UseDefaultPort
+						};
 
-					envelope = _ as BasicDeliverEventArgs;
-					var message = FromJson(envelope.Body);
 
-					lock (Lock)
+					using (var conn = factory.CreateConnection())
+					using (var channel = conn.CreateModel())
 					{
-						_actionsToPerformOnRead.ForEach(x => x(message));
+
+						channel.ExchangeDeclare(ExchangeName, ExchangeType.Topic, true, false, null);
+
+						var queueName = String.Empty;
+						//queue will be given random name, since it's non-durable we don't need to have a known queue name
+						var durable = false;
+						var exclusive = true;
+						var autoDelete = true;
+						var noAck = true; //these are non-critical and don't need ack.  Avoiding ack saves server and client some work
+
+
+						//durable (true) -> the queue will be persisted across a rabbitMQ node reboot
+						//exclusive (true) -> the queue can only be accessed via the current connection
+						//auto-delete (true) -> the queue will be deleted once the client disconnects
+						var queue = channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+						var routingKey = "ApiWebRequestCompletedEvent";
+
+						channel.QueueBind(queueName, ExchangeName, routingKey);
+
+						var consumer = new QueueingBasicConsumer(channel);
+						channel.BasicConsume(queueName, noAck, consumer);
+
+						BasicDeliverEventArgs envelope;
+
+						while (true)
+						{
+							object _ = null;
+							if (!consumer.Queue.Dequeue(500, out _)) //release poll at 500 ms
+								continue;
+
+							envelope = _ as BasicDeliverEventArgs;
+							var message = FromJson(envelope.Body);
+
+							lock (Lock)
+							{
+								_actionsToPerformOnRead.ForEach(x => x(message));
+							}
+						}
 					}
 				}
-
+				catch (Exception)
+				{
+					//this is a demo app, nothing fancy
+				}
 			}
 		}
 
